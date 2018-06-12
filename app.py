ee077fe67353c635f8666aafbaab177e11e40e13
Abhijith Rajan <arajan@stsci.edu>
2018-06-12 18:21:00 -0400

    Updates to app based on Zachs suggestions

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+#===========================================================================================
+# Milestone project for "The Data Incubator"
+
+# Written by
+# Abhijith Rajan
+# 07/12/2018
+
+#===========================================================================================
 from flask import Flask, render_template, request, redirect
 
 #from dotenv import load_dotenv, find_dotenv
@@ -11,57 +19,71 @@ import pandas as pd
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, HoverTool
 from bokeh.embed import components
+#===========================================================================================
 
 app = Flask(__name__)
-app.vars = {}
 
-
+#===========================================================================================
 @app.route('/', methods=['GET','POST'])
 def index():
+    pars = {}
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
-        app.vars['symbl'] = request.form['ticker']
+        pars['symbl'] = request.form['ticker']
         try:
-            app.vars['start'] = datetime.strptime(request.form['start'], "%Y-%m-%d")
+            pars['start'] = datetime.strptime(request.form['start'], "%Y-%m-%d")
         except:
             return render_template("error_time_stamp.html",errors="wrong datetime format")
         try:
-            app.vars['stop'] = datetime.strptime(request.form['stop'], "%Y-%m-%d")
+            pars['stop'] = datetime.strptime(request.form['stop'], "%Y-%m-%d")
         except:
             return render_template("error_time_stamp.html",errors="the wrong datetime format")
-        if app.vars['start'] >= app.vars['stop']:
+        if pars['start'] >= pars['stop']:
             return render_template("error_time_stamp.html",errors="the start date is great than the stop date")
-        if app.vars['stop'] > datetime(2018,3,27):
+        if pars['stop'] > datetime(2018,3,27):
             return render_template("error_time_stamp.html",errors="the stop date is after 2018-03-27")
 
-        app.vars['features'] = request.form.getlist('features')
-        return redirect('/plot')
+        pars['features'] = request.form.getlist('features')
 
-@app.route('/plot')
-def make_plot():
-    query_url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES?'
+        df = get_data(pars)
+
+        plot = make_plot(df, pars)
+
+        script, div = components(plot)
+
+        return render_template("graph.html", symbol=pars['symbl'], the_div=div, the_script=script)
+
+
+#===========================================================================================
+def get_data(pars):
+    query_url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES'
     quandl_api_key = os.environ.get('QUANDL_API_KEY')
-    stock_name = app.vars['symbl']
+    stock_name = pars['symbl']
 
-    start = app.vars['start'] #datetime.datetime.now()
-    stop = app.vars['stop'] #end + dateutil.relativedelta.relativedelta(months=-12)
+    start = pars['start'] #datetime.datetime.now()
+    stop = pars['stop'] #end + dateutil.relativedelta.relativedelta(months=-12)
 
     date_label = ""
     inc_date = start
     while inc_date <= stop:
-        print('{:%Y-%m-%d},'.format(inc_date))
         date_label += '{:%Y-%m-%d},'.format(inc_date)
         inc_date += timedelta(1)
 
-    query = "{}ticker={}&date={}&api_key={}".format(query_url,stock_name,date_label[:-1],quandl_api_key)
-
-    r = requests.get(query)
+    payload = {'ticker':stock_name, 'date':date_label[:-1], 'api_key':quandl_api_key}
+    r = requests.get(query_url, params=payload)
     data = r.json()
 
     colnames = [item['name'] for item in data['datatable']['columns']]
     df = pd.DataFrame(data['datatable']['data'], columns=colnames)#.set_index())
     df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+
+    return df
+
+#===========================================================================================
+def make_plot(df, pars):
+    features = pars['features']
+    stock_name = pars['symbl']
 
     source = ColumnDataSource(data={
         'date'    : df['date'],
@@ -73,8 +95,7 @@ def make_plot():
     })
 
 
-    hover = HoverTool(tooltips=[('Price', '@close')
-                               ])
+    hover = HoverTool(tooltips=[('Price', '@close')])
 
     p = figure(title='Quandl WIKI Stock Price for '+stock_name, 
                x_axis_label='Date', x_axis_type='datetime',
@@ -83,36 +104,35 @@ def make_plot():
                tools=[hover]
               )
 
-    if app.vars['features'] == []:
+    if not features:
         p.circle('date', 'close', size=3, alpha=0.8, source=source, color='blue')
         p.line('date', 'close', source=source, color='blue', legend='Closing price')
     
-    if 'open' in app.vars['features']:
+    if 'open' in features:
         p.circle('date', 'open', size=3, alpha=0.8, source=source, color='red')
         p.line('date', 'open', source=source, color='red', legend='Opening price')
 
-    if 'close' in app.vars['features']:
+    if 'close' in features:
         p.circle('date', 'close', size=3, alpha=0.8, source=source, color='blue')
         p.line('date', 'close', source=source, color='blue', legend='Closing price')
 
-    if 'adj_opn' in app.vars['features']:
+    if 'adj_opn' in features:
         p.circle('date', 'adj_open', size=3, alpha=0.8, source=source, color='black')
         p.line('date', 'adj_open', source=source, color='black', legend='Adjusted opening price')
 
-    if 'adj_clos' in app.vars['features']:
+    if 'adj_clos' in features:
         p.circle('date', 'adj_close', size=3, alpha=0.8, source=source, color='green')
         p.line('date', 'adj_close', source=source, color='green', legend='Adjusted closing price')
 
     p.legend.location = 'top_left'
 
-    script, div = components(p)
+    return p
 
-    return render_template("graph.html", symbol=stock_name, the_div=div, the_script=script)
-
+#===========================================================================================
 @app.route('/about')
 def about():
   return render_template('about.html')
 
-
+#===========================================================================================
 if __name__ == '__main__':
   app.run(host='0.0.0.0',port=33507, debug=True)
